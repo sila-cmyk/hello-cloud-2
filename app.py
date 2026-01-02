88db1d8e76fabbc01ad72c11f7eb41f0e9a94e42
sila-cmyk <slazorlu224@gmail.com>
2026-01-02 20:36:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,14 @@ from flask import Flask, render_template_string, request
 import os
 import psycopg2
 
-app = Flask (__name__)
+app = Flask(__name__)
 
-#Render'ın otomatik tanımladığı veritabanı bağlantı bilgisi (DATABASE_URL ortam değişkeni)
+# Render DATABASE_URL
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "postgresql://hello_cloud2_db_zfvo_user:qSEyyY97p1Iub4UKRvYeH2QxhtNohmb0@dpg-d3tjhbvdiees73dk0ghg-a.oregon-postgres.render.com/hello_cloud2_db_zfvo"
 )
 
-#HTML ŞABLONU (tek sayfada form + liste)
 HTML = """
 <!doctype html>
 <html>
@@ -28,50 +27,59 @@ HTML = """
 </head>
 <body>
     <h1>Buluttan Selam!</h1>
-    <p> Adını Yaz, selamını bırak:</p>
-    <form method= "POST">
+    <p>Adını yaz, selamını bırak</p>
+
+    <form method="POST">
         <input type="text" name="isim" placeholder="Adını Yaz" required>
         <button type="submit">Gönder</button>
     </form>
+
     <h3>Ziyaretçiler:</h3>
     <ul>
-        {% for ad in isimler%}
-             <li>{{ ad }}</li>
-        {% enfor %}
+        {% for ad in isimler %}
+            <li>{{ ad }}</li>
+        {% endfor %}
     </ul>
-</body>    
+</body>
 </html>
 """
 
 def connect_db():
     return psycopg2.connect(DATABASE_URL)
 
-
 @app.route("/", methods=["GET", "POST"])
 def index():
-   conn = connect_db()
-   cur = conn.cursor()
+    conn = connect_db()
+    cur = conn.cursor()
 
-    # Tablo yoksa oluştur 
-   cur.execute("CREATE TABLE IF NOT EXISTS ziyaretciler (id SERIAL PRIMARY KEY, isim TEXT)") 
+    # Tablo yoksa oluştur
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS ziyaretciler (
+            id SERIAL PRIMARY KEY,
+            isim TEXT
+        )
+    """)
 
-  #POST isteği geldiğinde formdan isim al ve kaydet  
-if request.method == "POST":
-      isim = request.form.get("isim")
-      if isim:
-         cur.execute("INSERT INTO ziyaretciler (isim VALUES (%s)", (isim,))
-         conn.commit()
-          
-#Ziyaretçileri sırala 
-cur.execute("SELECT isim FROM ziyaretciler ORDER BY id DESC LIMIT 10")
-isimler = [row[0] for row in cur.fetchall()]
+    # POST isteği
+    if request.method == "POST":
+        isim = request.form.get("isim")
+        if isim:
+            cur.execute(
+                "INSERT INTO ziyaretciler (isim) VALUES (%s)",
+                (isim,)
+            )
+            conn.commit()
 
-#Bağlantıyı kapat
-cur.close()
-conn.close()
+    # Ziyaretçileri al
+    cur.execute(
+        "SELECT isim FROM ziyaretciler ORDER BY id DESC LIMIT 10"
+    )
+    isimler = [row[0] for row in cur.fetchall()]
 
-#sayfayı render et 
-return render_template_string(HTML, isimler=isimler)
+    cur.close()
+    conn.close()
+
+    return render_template_string(HTML, isimler=isimler)
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5000)
+    app.run()
