22197f9d01d16d238b6dd6b27ee6857ad488410d
sila-cmyk <slazorlu224@gmail.com>
2026-01-03 04:02:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -76,4 +76,6 @@ def index():
     return render_template_string(HTML, isimler=isimler)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
